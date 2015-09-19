925bd55813ec8a59aa1d2a12529115eed2ac9b82
Emiliano Carvalho <emilianocarvalho.it@gmail.com>
2015-09-19 17:33:14 -0300

    Questão 4 Servidores Web

--- CalcularIMC/src/br/com/estacio/servlet/Dados.java
+++ CalcularIMC/src/br/com/estacio/servlet/Dados.java
@@ -17,13 +17,6 @@ import javax.servlet.http.HttpServletResponse;
 public class Dados extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public Dados() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
--- CalcularIMC/src/br/com/estacio/servlet/GetDados.java
+++ CalcularIMC/src/br/com/estacio/servlet/GetDados.java
@@ -18,14 +18,6 @@ public class GetDados extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public GetDados() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
