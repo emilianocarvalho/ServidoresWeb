b1ee72b075dbd8cfc59255b7db88164f0ca5a678
Emiliano Carvalho <emilianocarvalho.it@gmail.com>
2015-10-25 23:24:05 -0300

    Fornecedor DAO list

--- BloqueioTelemarketing/src/br/gov/pb/procon/dao/FornecedorDAO.java
+++ BloqueioTelemarketing/src/br/gov/pb/procon/dao/FornecedorDAO.java
@@ -4,9 +4,14 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import br.gov.pb.procon.jdbc.ConnectionFactory;
+import br.gov.pb.procon.model.Consumidor;
 import br.gov.pb.procon.model.Fornecedor;
 
 import com.mysql.jdbc.PreparedStatement;
@@ -15,7 +20,7 @@ public class FornecedorDAO {
 	private Connection connection;
 
 	/**
-	 * 
+	 * construct
 	 * @throws SQLException
 	 * @throws ClassNotFoundException
 	 */
@@ -23,6 +28,12 @@ public class FornecedorDAO {
 		this.connection = new ConnectionFactory().getConnection();
 	}
 
+	/**
+	 * adiciona
+	 * @param fornecedor
+	 * @throws SQLException
+	 * @throws UnknownHostException
+	 */
 	public void adiciona(Fornecedor fornecedor) throws SQLException, UnknownHostException {
 		try {
 			String sql = "INSERT INTO fornecedor" +
@@ -47,5 +58,41 @@ public class FornecedorDAO {
 			throw new RuntimeException(e);
 		}
 	}
+
+	/**
+	 * lista
+	 * @return
+	 */
+	public List<Fornecedor> getLista() {
+		try {
+			List<Fornecedor> fornecedores = new ArrayList<Fornecedor>();
+			PreparedStatement stmt = (PreparedStatement) this.connection.prepareStatement("select * from fornecedor");
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				Fornecedor fornecedor = new Fornecedor();
+				fornecedor.setFn_id(rs.getLong("fn_id"));
+				fornecedor.setFn_razao_social(rs.getString("fn_razao_social"));
+				fornecedor.setFn_cnpj(rs.getString("fn_cnpj"));
+				fornecedor.setFn_email(rs.getString("fn_email"));
+				fornecedor.setFn_telb4(rs.getString("fn_telb4"));
+				fornecedor.setFn_ativo(rs.getInt("fn_ativo"));
+				fornecedor.setFn_lastip(rs.getString("fn_lastip"));	
+
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("fn_lastlogin"));
+				fornecedor.setFn_lastlogin(data);
+
+				fornecedores.add(fornecedor);
+			}
+			rs.close();
+			stmt.close();
+			return fornecedores;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+	
 
 }
